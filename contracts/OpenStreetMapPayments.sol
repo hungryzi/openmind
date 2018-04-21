ac0fd45fb7d7b2132ebe68add36fdb7b6955e5f6
Zi <hungryzi@gmail.com>
2018-04-21 17:48:44 -0400

    Updated contracts

--- contracts/OpenStreetMapPayments.sol
+++ contracts/OpenStreetMapPayments.sol
@@ -14,11 +14,12 @@ contract OpenStreetMapPayments is PullPayment {
   using SafeMath for uint256;
 
   function fund() public payable {
+    require(msg.value > 0);
     address(this).transfer(msg.value);
   }
 
-  function getPayment(address _contributor) public view returns (uint256) {
-    return payments[_contributor];
+  function getPayment() public view returns (uint256) {
+    return payments[msg.sender];
   }
 
   function reportContributions(address _contributor, uint256 _wei) public {
