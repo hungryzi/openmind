9bfa18bc5b81466c6051554bde95ba467661b20d
Zi <hungryzi@gmail.com>
2018-04-22 02:59:46 -0400

    Updated index

--- dapps/scripts/index.js
+++ dapps/scripts/index.js
@@ -19,22 +19,48 @@ window.prepareTestData = function() {
     }
   ];
 
-  localStorage.setItem("contributors", JSON.stringify(contributors));
+  localStorage.clear();
+  contributors.forEach(function(contributor) {
+    localStorage.setItem(contributor.user, JSON.stringify(contributor));
+  });
 };
 
 window.renderContributorsTable = function() {
-  var contributors = JSON.parse(
-    localStorage.getItem("contributors") || JSON.stringify([])
-  );
-
   $("#contributorsTable tbody").empty();
-  contributors.forEach(function(contributor) {
+
+  var contributorsCount = localStorage.length;
+  for (var i = 0; i < contributorsCount; i++) {
+    var user = localStorage.key(i);
+    var contributor = JSON.parse(localStorage.getItem(user));
+
     var $row = $("<tr/>");
     $row.append($("<td/>").text(contributor.user));
     $row.append($("<td/>").text(contributor.address));
     $row.append($("<td/>").text(contributor.pendingContributions));
 
     $("#contributorsTable tbody").append($row);
+  }
+};
+
+window.updateContributions = function() {
+  var response = [
+    { user: "hungryzi", contributionsCount: getRandomInt(2) },
+    { user: "cuminh", contributionsCount: getRandomInt(4) },
+    { user: "tien", contributionsCount: getRandomInt(3) },
+    { user: "franscis", contributionsCount: getRandomInt(5) },
+    { user: "jishnu", contributionsCount: getRandomInt(7) },
+    { user: "cat", contributionsCount: getRandomInt(8) }
+  ];
+
+  parseResponse(response);
+};
+
+window.parseResponse = function(contributors) {
+  contributors.forEach(function(contributor) {
+    var user = contributor.user;
+    var contributor = JSON.parse(localStorage.getItem(user) || "{}");
+
+    if
   });
 };
 
