1da12c0defa87b7801ee6ef6db10a72b1a75eb47
Tien Phan <fanngoctien@gmail.com>
2018-04-22 04:02:55 -0400

    change menu

---
+++ dapps/scripts/datahandler.js
@@ -0,0 +1,13 @@
+
+// read file: test.json
+var data = JSON.Parse(data)
+var l = data.length;
+var contributions = {};
+for(var i = 0; i < l; i++) {
+  data[0]._id = // username
+  // check exist
+  // if not exist:
+  contributions["_id"] = 1;
+  // if exist:
+  contributions["_id"] += 1;
+}
